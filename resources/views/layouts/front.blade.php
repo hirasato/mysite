4e44e151f77fbeb364eb1e9568bb0280e17f8001
hirasato <hirasato19@icloud.com>
2022-02-01 01:44:53 +0000

    massurucommit9

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -27,53 +27,6 @@
     </head>
     <body>
         <div id="app">
-            {{-- 画面上部に表示するナビゲーションバー。 --}}
-            <!--<nav class="navbar navbar-expand-md navbar-dark navbar-laravel">-->
-            <!--    <div class="container">-->
-            <!--        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">-->
-            <!--            <span class="navbar-toggler-icon"></span>-->
-            <!--        </button>-->
-
-            <!--        <div class="collapse navbar-collapse" id="navbarSupportedContent">-->
-                        <!-- Left Side Of Navbar -->
-                        <!--<ul class="navbar-nav mr-auto">-->
-                        <!--   @if (Route::has('register'))-->
-                        <!--            <a href="{{ route('register') }}">Register</a>-->
-                        <!--   @endif-->
-                        <!--</ul>-->
-
-                        <!-- Right Side Of Navbar -->
-                        <!--<ul class="navbar-nav ml-auto">-->
-                              <!-- Authentication Links -->
-                        <!--{{-- ログインしていなかったらログイン画面へのリンクを表示 --}}-->
-                        <!--@guest-->
-                        <!--    <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>-->
-                        <!--{{-- ログインしていたらユーザー名とログアウトボタンを表示 --}}-->
-                        <!--@else-->
-                        <!--    <li class="nav-item dropdown">-->
-                        <!--        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>-->
-                        <!--            {{ Auth::user()->name }} <span class="caret"></span>-->
-                        <!--        </a>-->
-
-                        <!--        <div class="dropdown-menu" aria-labelledby="navbarDropdown">-->
-                        <!--            <a class="dropdown-item" href="{{ route('logout') }}"-->
-                        <!--               onclick="event.preventDefault();-->
-                        <!--                             document.getElementById('logout-form').submit();">-->
-                        <!--                {{ __('Logout') }}-->
-                        <!--            </a>-->
-
-                        <!--            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">-->
-                        <!--                @csrf-->
-                        <!--            </form>-->
-                        <!--        </div>-->
-                        <!--    </li>-->
-                        <!--    @endguest-->
-                        <!--</ul>-->
-            <!--        </div>-->
-            <!--    </div>-->
-            <!--</nav>-->
-            <!--{{-- ここまでナビゲーションバー --}}-->
-
             <main class="py-4">
                 {{-- コンテンツをここに入れるため、@yieldで空けておきます。 --}}
                 @yield('content')
